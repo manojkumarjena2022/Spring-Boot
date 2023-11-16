5a65b21a99ce7d461b112d2e3249655c24dd77ed
Manoj <manojkumarjena2022@gmail.com>
2023-11-16 07:16:17 +0530

    injecting all type values to spring bean properties using yml file

---
+++ BootProj9-InjectingValuesAllTypeOfSpringBeanYML/src/test/java/com/mj/BootProj8InjectingValuesAllTypeOfSpringBeanPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProj8InjectingValuesAllTypeOfSpringBeanPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
